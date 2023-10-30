d3e2017f6ddecae53e2001515f06d43a4824b53e
koralle <mkhd7930@gmail.com>
2023-10-30 14:31:19 +0900

    chore(domains/vitest): resolve not relative path
    see: https://vitest.dev/guide/common-errors.html

--- packages/domains/vitest.config.ts
+++ packages/domains/vitest.config.ts
@@ -1,5 +1,11 @@
-import { defineProject } from "vitest/config"
+import { defineProject } from "vitest/config";
 
-const config = defineProject({})
+const config = defineProject({
+  test: {
+    alias: {
+      "@/": new URL("./src/", import.meta.url).pathname,
+    },
+  },
+});
 
-export default config
+export default config;
